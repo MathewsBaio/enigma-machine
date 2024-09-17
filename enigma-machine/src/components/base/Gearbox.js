ed2d2b45d62e00acc3fc8ccd9d60c7222e9ef296
MathewsBaio <baiomathews@gmail.com>
2024-09-16 23:32:27 -0300

    .

--- enigma-machine/src/components/base/Gearbox.js
+++ enigma-machine/src/components/base/Gearbox.js
@@ -3,20 +3,72 @@ export default function Gearbox() {
     <div className="config-gearbox">
       <div className="configuration">
         <h1>Left Rotor</h1>
-        <div>
+        <div className="configuration-options">
           <label>Model</label>
-          <select></select>
+          <select>
+            <option>I</option>
+            <option>II</option>
+            <option>III</option>
+            <option>IV</option>
+            <option>V</option>
+          </select>
         </div>
-        <div>
+        <div className="configuration-options">
           <label>Ring</label>
-          <select></select>
+          <select>
+            <option>I</option>
+            <option>II</option>
+            <option>III</option>
+            <option>IV</option>
+            <option>V</option>
+          </select>
         </div>
       </div>
       <div className="configuration">
         <h1>Midle Rotor</h1>
+        <div className="configuration-options">
+          <label>Model</label>
+          <select>
+            <option>I</option>
+            <option>II</option>
+            <option>III</option>
+            <option>IV</option>
+            <option>V</option>
+          </select>
+        </div>
+        <div className="configuration-options">
+          <label>Ring</label>
+          <select>
+            <option>I</option>
+            <option>II</option>
+            <option>III</option>
+            <option>IV</option>
+            <option>V</option>
+          </select>
+        </div>
       </div>
       <div className="configuration">
         <h1>Right Rotor</h1>
+        <div className="configuration-options">
+          <label>Model</label>
+          <select>
+            <option>I</option>
+            <option>II</option>
+            <option>III</option>
+            <option>IV</option>
+            <option>V</option>
+          </select>
+        </div>
+        <div className="configuration-options">
+          <label>Ring</label>
+          <select>
+            <option>I</option>
+            <option>II</option>
+            <option>III</option>
+            <option>IV</option>
+            <option>V</option>
+          </select>
+        </div>
       </div>
     </div>
   );
